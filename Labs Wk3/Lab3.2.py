1665273c1295a4d1bb0aa7e0e96658c283607fa1
Paul Caulfield <SESA87346@se.com>
2020-10-10 16:36:28 +0100

    Uploaded Part 3 of labs for week 3 - web scraping

---
+++ Labs Wk3/Lab3.2.py
@@ -0,0 +1,56 @@
+# Paul Caulfield, 2020
+import requests 
+# Import Request Module
+import csv
+from bs4 import BeautifulSoup
+# import Beautiful Soup package for navigating HTML & XML DOM trees, use to prettify 
+with open("carviewer.html") as fp:
+    # open file and send contents to Beautiful Soup as fp
+    soup = BeautifulSoup(fp, 'html.parser')
+    # parse the file and stores the DOM tree as 'soup'
+#print(soup.prettify())
+# print DOM tree
+#print (soup.tr)
+# print 1st occurrence of element which contains tr tagline
+employee_file = open('week03data.csv', mode='w') 
+# open file and name it 'employee.csv and set the file mode = w for write - to create blank csv file
+employee_writer = csv.writer(employee_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+# make virable called employee.writer which uses csv writer to write to (filename,delimite and other paramters)
+
+
+
+#rows = soup.findAll("tr")
+# return a list of elements with tr tagline and sore in memory
+#for row in rows:
+#   iterate through rows  
+    #print(row)
+    # print rows
+    #dataList = []
+    # create an array call dataList 
+    #cols = row.findAll("td")
+    # return a list of elements with td tagline and sore in cols
+
+    #for col in cols:
+    # iterate through cols
+        #dataList.append(col.text)
+        # append text element into array dataList
+    #employee_writer.writerow(dataList)
+    # to write row, pass list  
+
+listings = soup.findAll("table", class_="table" )
+
+for listing in listings:
+    entryList = []
+    
+    reg = listing.find(th="Reg").text
+    entryList.append(reg)
+    make = listing.find(th="Make").text
+    entryList.append(make)
+    model = listing.find(th="Model").text
+    entryList.append(model)
+    price = listing.find(th="Price").text
+    entryList.append(price)
+
+    employee_writer.writerow(entryList)
+
+    
--- Labs Wk3/Lab3.py
+++ Labs Wk3/Lab3.py
@@ -1,15 +1,44 @@
 # Paul Caulfield, 2020
-# Getting Content Into Program
+# Write a program that stores the data for all trains in Ireland in a csv file
+# Use the Irish rail API http://api.irishrail.ie/realtime/realtime.asmx/getCurrentTrainsXML to retrieve the data.
 import requests 
 # Import Request Module
+import csv
 from bs4 import BeautifulSoup
 # import Beautiful Soup package for navigating HTML & XML DOM trees, use to prettify 
-page = requests.get("http://dataquestio.github.io/web-scraping-pages/simple.html")
+url = "http://api.irishrail.ie/realtime/realtime.asmx/getCurrentTrainsXML"
+# url = Irish rail API
+page = requests.get(url)
 # use requests.get to pass url
-soup1 = BeautifulSoup(page.content, 'html.parser')
-print(page)
-# PPrint page
-print("----------------")
-#print(page.content)
-# Print content
-print(soup1.prettify())
+soup = BeautifulSoup(page.content, 'xml')
+# parse the file using xml parser as API is in XML and stores the DOM tree as 'soup'
+# print(soup.prettify()) 
+retrieveTags=['TrainStatus', 'TrainLatitude', 'TrainLongitude', 'TrainCode', 'TrainDate', 'PublicMessage', 'Direction' ]
+
+with open('week03_train.csv', mode='w') as train_file: 
+# open csv file
+    train_writer = csv.writer(train_file, delimiter='\t', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    listings = soup.findAll("objTrainPositions") 
+    # find the listings ie all elements = objTrainPositions
+    for listing in listings: 
+    # iterate through the listing in order to print each out
+        lat =float( listing.TrainLatitude.string) 
+        #get the latitude of this train and store it as a float
+        if (lat < 53.4):
+        # then check if it is less then the latitude of Dublin 53.4
+            #print(listing)
+            #print(listing.TrainLatitude.string)
+            #print(listing.find('TrainLatitude').string)
+            # print the content of the element TrainLatitude
+            entryList = [] 
+            # create an array called entryList
+            for retrieveTag in retrieveTags:
+            #for loop to iterate through these tag names.
+                #print (listing.find(retrieveTag).string) 
+                entryList.append(listing.find(retrieveTag).string)
+                #append in the latitude.
+                train_writer.writerow(entryList)
+                # write data to the CSV.
+
+train_file.close()
+# close file
